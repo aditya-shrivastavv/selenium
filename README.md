57190f495adfa78ef8e92e8666d0d716bed921f6
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-09-07 08:49:38 +0530

    learnt about implict and explict wait

--- README.md
+++ README.md
@@ -2,10 +2,30 @@
 
 ## Methods
 
-### Implicit Wait
+### Implicit Wait and Explict Wait
+
+#### Implict
 
 ```py
 driver.implicitly_wait(10)
 ```
 
 Sets a sticky timeout to implicitly wait for an element to be found, or a command to complete. This method only needs to be called one time per session.
+
+#### Explicit
+
+```py
+from selenium.webdriver.support.ui import WebDriverWait
+
+WebDriverWait(driver=driver, timeout=30).until(
+    EC.text_to_be_present_in_element(
+        (By.CLASS_NAME, "progress-label"),  # Getting the element
+        "Complete!",  # Expected text
+    )
+)
+```
+
+#### Implicit Wait VS Explicit Wait
+
+- We can use implicit wait to find elements in the page
+- We can use explicit wait to wait for some time untill some condition is met
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 # setting an environment variable in system for the runtime
-os.environ['PATH'] += r"E:/Drivers/SeleniumDrivers"
+os.environ["PATH"] += r"E:/Drivers/SeleniumDrivers"
 
 # workaround to make the browser stay open
 options = webdriver.ChromeOptions()
@@ -10,13 +14,23 @@ options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=options)
 
+
 def main():
     driver.get("https://jqueryui.com/resources/demos/progressbar/download.html")
-    
+
     # Wait for the website to load properly, wait 3 secs.
     driver.implicitly_wait(10)
-    
-    downloadBtn = driver.find_element("id", "downloadButton")
+
+    downloadBtn = driver.find_element(By.ID, "downloadButton")
     downloadBtn.click()
+
+    # Explicit wait untill the progress is completed.
+    WebDriverWait(driver=driver, timeout=30).until(
+        EC.text_to_be_present_in_element(
+            (By.CLASS_NAME, "progress-label"),  # Getting the element
+            "Complete!",  # Expected text
+        )
+    )
+
 
 main()
