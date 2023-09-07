bda061b0ccc52f03521bffe3a92524bb726fa224
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-09-07 12:54:42 +0530

    keys can also be pressed to do something

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 # setting an environment variable in system for the runtime
 os.environ["PATH"] += r"E:/Drivers/SeleniumDrivers"
@@ -34,7 +35,8 @@ def main():
     inputField2.clear()
     inputField2.send_keys(99)
 
-    submitButton.click()
+    # submitButton.click()
+    submitButton.send_keys(Keys.ENTER)
 
     result = outputField.get_attribute("value")
     print("Result:", result)
